abff3a7e3c25874f2ce1543d04e5b601dd753f5a
Emanuele Moncada <em@zerowk.it>
2023-02-06 19:49:31 +0100

    update

--- index.js
+++ index.js
@@ -24,16 +24,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Cross Origins
-/* app.use((req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-}); */
-j
 //Entrypoint
 app.use('/characters', charactersRoutes)
 

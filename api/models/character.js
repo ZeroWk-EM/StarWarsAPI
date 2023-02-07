84a2548b63308fd1a0d22b7b17e468a0b88d75dc
wkasper <em@zerowk.it>
2023-02-07 12:50:56 +0100

    add character DB schema

--- api/models/character.js
+++ api/models/character.js
@@ -2,6 +2,28 @@ const mongoose = require('mongoose');
 
 const characterSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    birth_year: { type: String, required: true },
+    species: { type: String, required: true },
+    gender: {
+        type: String,
+        enum: {
+            values: ['Male', 'Famale', 'Other', 'Unknown', 'N/A'],
+            message: '{VALUE} is not supported, see the documentation for see acceptable values'
+        }
+    },
+    eye_color: { type: String, required: true },
+    height: { type: Number, required: true },
+    weight: { type: Number, required: true },
+    skin_color: { type: String, required: true },
+    organization: { type: [String], index: true },
+    homeworld: { type: String, required: true },
+    films: { type: [String], index: true },
+    series: { tyoe: [String], index: true },
+    starships: { type: [String], index: true },
+    vehicles: { type: [String], index: true },
+    weapons: { type: [String], index: true }
 });
 
 module.exports = mongoose.model('character', characterSchema);

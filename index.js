38e7b47aa8ab615eba695188beec288585cd29f9
wkasper <em@zerowk.it>
2023-02-07 11:12:09 +0100

    refector index.js

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-//Import Node Node Module
+//Importazione dei noduli di NODE
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-//Import delle routes
-const charactersRoutes = require('./api/routes/characters');
+
+// Stringe che contengono gli URL dei Middlware 
+const charactersRoutes = require('./api/routes/character');
 
 //Connessione al DB di Mongo
 mongoose.set('strictQuery', false);
 const stringConnection = 'mongodb://127.0.0.1:27017/starwars';
 mongoose.connect(stringConnection);
-const database = mongoose.connection;
+const connection = mongoose.connection;
 
-//Verico la connesione
-database.once('connected', () => {
+connection.on('connected', function() {
     console.log('\u001B[32m[StarWarsAPI] Database Connesso \x1b[0m');
-})
+});
 
-const app = express();
+connection.on('error', function(err) {
+    console.error('\u001B[31m[StarWarsAPI] Errore durante la connessione a MongoDB \x1b[0m', err);
+});
 
+
+// Middleware per registrare le richieste HTTP effettuate al server
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Entrypoint
+// Middleware per accadere agli URL
 app.use('/characters', charactersRoutes)
 
 //Porta
